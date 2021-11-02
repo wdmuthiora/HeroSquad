8a56a332d030bcaa87694e14fab8b3a2593bbeee
wdmuthiora <wdmuthiora@gmail.com>
2021-11-02 17:15:40 +0300

    Refactored the SquadTest.

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -16,21 +16,21 @@ class SquadTest {
     @Test
     @DisplayName("Squad Instantiates Correctly.")
     public void squad_instantiatesCorrectly() {
-        Squad avengers = new Squad("Avengers","Captain America", new String[]{"Captain America"},10,"Destroy Hydra");
+        Squad avengers = setUpSquad();
         assertTrue(avengers instanceof Squad);
 
     }
     @Test
     @DisplayName("Returns one instance of Heroes")
     public void all_returnsAllInstancesOfSquad_true() {
-        Squad justiceLeague = new Squad("Justice League", "Superman", new String[]{"Cyborg"}, 7, "Be as cool as the Avengers");
+        Squad justiceLeague = setUpSquad2();
         assertTrue(justiceLeague.allSquads().contains(justiceLeague));
     }
     @Test
     @DisplayName("Returns two instance of Heroes")
     public void all_returnsTwoInstancesOfSquad_true() {
-        Squad justiceLeague = new Squad("Justice League", "Superman", new String[]{"Cyborg"}, 7, "Be as cool as the Avengers");
-        Squad avengers = new Squad("Avengers","Captain America", new String[]{"Captain America"},10,"Destroy Hydra");
+        Squad justiceLeague = setUpSquad2();
+        Squad avengers = setUpSquad();
         assertTrue(justiceLeague.allSquads().contains(justiceLeague));
         assertTrue(avengers.allSquads().contains(avengers));
 
@@ -38,25 +38,32 @@ class SquadTest {
     @Test
     @DisplayName("Gets Squad's Name")
     public void get_SquadName_true(){
-        Squad justiceLeague = new Squad("Justice League", "Superman", new String[]{"Cyborg"}, 7, "Be as cool as the Avengers");
+        Squad justiceLeague = setUpSquad2();
         assertEquals("Justice League", justiceLeague.getSquadName());
     }
     @Test
     @DisplayName("Gets Squad's Leader")
     public void get_SquadLeader_true(){
-        Squad justiceLeague = new Squad("Justice League", "Superman", new String[]{"Cyborg"}, 7, "Be as cool as the Avengers");
+        Squad justiceLeague = setUpSquad2();
         assertEquals("Superman", justiceLeague.getSquadLeader());
     }
     @Test
     @DisplayName("Gets Squad's Size")
     public void get_SquadSize_true(){
-        Squad justiceLeague = new Squad("Justice League", "Superman", new String[]{"Cyborg"}, 7, "Be as cool as the Avengers");
+        Squad justiceLeague = setUpSquad2();
         assertEquals(7, justiceLeague.getSquadSize());
     }
     @Test
     @DisplayName("Gets Squad's Mission")
     public void get_SquadMission_true(){
-        Squad justiceLeague = new Squad("Justice League", "Superman", new String[]{"Cyborg"}, 7, "Be as cool as the Avengers");
+        Squad justiceLeague = setUpSquad2();
         assertEquals("Be as cool as the Avengers", justiceLeague.getSquadMission());
+    }
+    public Squad setUpSquad(){
+        return new Squad("Avengers","Captain America", new String[]{"Captain America"},10,"Destroy Hydra");
+    }
+    public Squad setUpSquad2(){
+        return new Squad("Justice League", "Superman", new String[]{"Cyborg"}, 7, "Be as cool as the Avengers");
+
     }
 }
