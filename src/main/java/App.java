0b9965422d10377e09c16dfe6af858f597083252
wdmuthiora <wdmuthiora@gmail.com>
2021-11-02 22:05:15 +0300

    Styled index and layout templates.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,14 +10,11 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-//        get("/layout", (request, response) -> { //request for route happens at this location
-//            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
-//            return new ModelAndView(model, "layout.hbs"); // assemble individual pieces and render
-//        }, new HandlebarsTemplateEngine());
-//
-//        get("/hello", (request, response) -> { //request for route happens at this location
-//            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
-//            return new ModelAndView(model, "hello.hbs"); // assemble individual pieces and render
-//        }, new HandlebarsTemplateEngine());
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("username", request.session().attribute("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
