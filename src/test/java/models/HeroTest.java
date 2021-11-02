d4cc32dcdcbfd73bed4fedce8ba53d428b03cc28
wdmuthiora <wdmuthiora@gmail.com>
2021-11-02 10:41:10 +0300

    Test if Hero instantiates correctly.

---
+++ src/test/java/models/HeroTest.java
@@ -0,0 +1,25 @@
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+    @Test
+    @DisplayName("Hero Instantiates Correctly.")
+    public void hero_instantiatesCorrectly() {
+        Hero thor = new Hero();
+        assertTrue(thor instanceof Hero);
+    }
+}
