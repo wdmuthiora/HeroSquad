27399147fa7f7d03d6f785cf70a2dde53ded00b1
wdmuthiora <wdmuthiora@gmail.com>
2021-11-04 03:39:36 +0300

    Added loop to display squads in squad-view template.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,6 +15,10 @@ public class App {
         Hero hulk =  new Hero("Hulk",21, "Durability","Anger","Avengers","The Abomination", 5 );
         Hero flash = new Hero("flash",30,"Super speed","no brakes","Justice League","Reverse Flash", 1000);
         Hero thor = new Hero("Thor", 2000, "Durability", "Pride", "Avengers","Loki",2000);
+        Squad justiceLeague = new Squad("Justice League", "Superman",  7, "Be as cool as the Avengers");
+        Squad avengers= new Squad("Avengers","Captain America", 10,"Destroy Hydra");
+
+
 
         //Create a Hero
         get("/heroes/new", (req, res) -> {
@@ -22,15 +26,29 @@ public class App {
             return new ModelAndView(model, "hero-form.hbs");
         }, new HandlebarsTemplateEngine());
 
+        //Create a Squad
+        get("/squad/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "squad-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
         //get all heroes
         get("/hero-view", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            List<Hero> heroes = Hero.allHeroes(); //From Hero class, get all hero instances and put them into the ArrayList.
-            model.put("hero", hulk);
-            System.out.println(hulk.id);
+            Map<String, ArrayList<Hero>> model = new HashMap<>();
+            ArrayList<Hero> heroes = Hero.allHeroes(); //From Hero class, get all hero instances and put them into the ArrayList.
             model.put("heroes", heroes);
+            System.out.println(thor.id);
             return new ModelAndView(model, "hero-view.hbs");
         }, new HandlebarsTemplateEngine());
+
+        //get all squad
+        get("/squad-view", (req, res) -> {
+            Map<String, ArrayList<Squad>> model = new HashMap<>();
+            ArrayList<Squad> squads = Hero.allHeroes(); //From Squad class, get all hero instances and put them into the ArrayList.
+            model.put("squads", squads);
+            return new ModelAndView(model, "squad-view.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
         //get hero by ID.
         get("/hero-view/:id", (req, res) -> {
@@ -41,5 +59,13 @@ public class App {
             return new ModelAndView(model, "hero-view.hbs"); //individual hero page.
         }, new HandlebarsTemplateEngine());
 
+        //get squad by ID.
+        get("/squad-view/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfSquadToFind = Integer.parseInt(req.params(":id")); //pull id - must match route segment
+            Squad foundSquad = Squad.findById(idOfSquadToFind); //use it to find squad
+            model.put("post", foundSquad); //add it to model for template to display
+            return new ModelAndView(model, "squad-view.hbs"); //individual squad page.
+        }, new HandlebarsTemplateEngine());
     }
 }
